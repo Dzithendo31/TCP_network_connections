4158aa4d01bb7c45f0933ccd0e0a2d7d80a74e7a
Reshowfrans <110479243+Reshowfrans@users.noreply.github.com>
2023-03-02 22:19:37 +0200

    Add files via upload

--- Sever.py
+++ Sever.py
@@ -1,90 +1,196 @@
-import os
-import socket
+import socket 
+import csv
+import hashlib
 import threading
+import os
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 1234
-ADDR = (IP, PORT)
-SIZE = 1024
+Port = 1222
+add = (IP, Port)
 FORMAT = "utf-8"
-SERVER_DATA_PATH = "server_data"
+SIZE = 1024
+buffer = 12551050
+
+#an Array to act as a Database :: 1. Name of File, 2. boolean Open or Locked 3. passCode ::default to 0000
+class File:
+    def __init__(self,name,open,pinCode):
+         self.name = name
+         self.open = open
+         self.pinCode = pinCode
 
 def handle_client(conn, addr):
+    my_files = []
+    send_to_array(my_files)
     print(f"[NEW CONNECTION] {addr} connected.")
-    conn.send("OK@Welcome to the File Server.".encode(FORMAT))
-
+        #connection has been Established here
     while True:
-        data = conn.recv(SIZE).decode(FORMAT)
-        data = data.split("@")
-        cmd = data[0]
 
-        if cmd == "LIST":
-            files = os.listdir(SERVER_DATA_PATH)
-            send_data = "OK@"
+            #in command the server expert like 
+            #upload/file_name.txt/c@0001
+            #download/file_name.txt/O
 
-            if len(files) == 0:
-                send_data += "The server directory is empty"
-            else:
-                send_data += "\n".join(f for f in files)
-            conn.send(send_data.encode(FORMAT))
+            #now lets handle this request message
+            command =conn.recv(SIZE).decode(FORMAT)
+            if len(command.split("/")) < 3:
+                conn.close()
+                print(f"[Disconnected] {addr} disconnected, Incorrect input on Client" )
+                break
+            comnd,filename,state = command.split("/")
 
-        elif cmd == "UPLOAD":
-            name, text = data[1], data[2]
-            filepath = os.path.join(SERVER_DATA_PATH, name)
-            with open(filepath, "w") as f:
-                f.write(text)
+            #now to handle the locked part
 
-            send_data = "OK@File uploaded successfully."
-            conn.send(send_data.encode(FORMAT))
-
-        elif cmd == "DOWNLOAD":
-            files = os.listdir(SERVER_DATA_PATH)
-            send_data = "OK@"
-            filename = data[1]
-
-            if len(files) == 0:
-                send_data += "The server directory is empty"
-            else:
-                if filename in files:
-                    with open(filename, "r") as f:
-                        text = f.read()
-
-                        send_data = f"{cmd}@{filename}"
-                        conn.send(send_data.encode(FORMAT))
-                        conn.send(text.encode(FORMAT))
-                        send_data += "File downloaded successfully."
+            if comnd == "upload":
+                if state[0] == "C":
+                    #then the file should be closed with passCode
+                    c,code = state.split("@")
+                    print(code)
+                    my_files.append(File(filename,"locked",code))
+                    save_file("data/saved.csv",filename,"locked",code)
                 else:
-                    send_data += "File not found."
+                    #this will be when the file is open
+                    my_files.append(File(filename,"open",0))
+                    save_file("data/saved.csv",filename,"open",0)
+                #then call the uploading stuff
+                #filename = conn.recv(SIZE).decode(FORMAT)
+                upload(filename,conn)
+            elif comnd == "query":
+                if len(my_files) == 0:
+                    #if the array is empty there are no files in the server
+                    conn.send("No files in server".encode(FORMAT))
+                else:
+                    #print them all to the client server
+                    String = ""
+                    String = query(my_files, String)
+                    conn.send(String.encode(FORMAT))
+                    #the string is sent to user
+                    print("There are Files")
+            elif comnd == "download":
+                #this where it gonna check even the passwords and stuff
+                code = state
+                #method to search for file name in protocol
 
-            conn.send(send_data.encode(FORMAT))
+                #search for array 
+                ans = search_array(my_files, filename)
+                if ans == None:
+                    #sends a message to server that file was not found
+                    conn.send("NONE".encode(FORMAT))
+                else:
+                    #look into password now
+                    if ans.pinCode == code:
+                        #sends a message to server that file was found
+                        conn.send("FOUND".encode(FORMAT))
+                        siZe = os.path.getsize(f"data/{filename}")
+                        download(filename,conn,addr,siZe)
+                    else:
+                        #sends message that file was found but incorrect password
+                        #for security reason this is the same code as when the file was not found
+                        conn.send("NONE".encode(FORMAT))
 
-        elif cmd == "LOGOUT":
-            break
-        elif cmd == "HELP":
-            data = "OK@"
-            data += "LIST: List all the files from the server.\n"
-            data += "UPLOAD <path>: Upload a file to the server.\n"
-            data += "DOWNLOAD <filename>: Download a file from the server.\n"
-            data += "LOGOUT: Disconnect from the server.\n"
-            data += "HELP: List all the commands."
 
-            conn.send(data.encode(FORMAT))
+            else:
+                conn.close()
+                print(f"[Disconnected] {addr} disconnected." )
 
-    print(f"[DISCONNECTED] {addr} disconnected")
-    conn.close()
 
+        #print(filename)
+
+    
 def main():
-    print("[STARTING] Server is starting")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
-    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+    #this is the array for storing our File objects
+    print("[STARTING] Sever is starting.")
+    sever = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sever.bind(add)
+    sever.listen()
+    print("[LISTENING] Sever is listening.")
 
     while True:
-        conn, addr = server.accept()
+        conn, addr = sever.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        
 
-if __name__ == "__main__":
+
+def upload(file_name,conn):
+        print("[RECV] Filename received")
+        #file = open(f"data/{file_name}", "wb")
+        conn.send("Filename received".encode(FORMAT))
+
+        SizeX = int(conn.recv(SIZE).decode(FORMAT))
+        #data = bytes(data, encoding='utf-8')
+        sent = 0
+        with open(f"data/{file_name}", 'wb') as f:
+            
+            while sent<SizeX:
+                data = conn.recv(SizeX)#.decode(FORMAT)
+                f.write(data)
+                sent += len(data)
+
+            f.close()
+        
+        print("[RECV] Filename Data received")
+        conn.send("File data received".encode(FORMAT))
+
+
+        #Receive hash 
+        received_file_hash = conn.recv(64).decode()
+        #Create Hash
+        computed_file_hash = hashlib.sha256(data).hexdigest()
+        
+        #Compare two hashes
+        if received_file_hash == computed_file_hash:
+            print('File has been transmitted successfully')
+            conn.send("File received Unchanged!".encode(FORMAT))
+        else:
+            print('File has been corrupted during transmission')
+
+        
+def download(file_name,client,addr,size):
+    #this is the method to download files from this server
+    #two parameterfile name and the socket connection
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[Client]: {msg}")
+
+    #get file size
+    file_size = size
+    client.send(str(file_size).encode(FORMAT))
+    
+    sent = 0 #bytes
+
+
+    with open(f"data/{file_name}", 'rb') as f:
+
+        while sent<file_size:
+            bytes_read = f.read(file_size)
+            client.sendall(bytes_read)
+            sent += len(bytes_read)
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[Client]: {msg}")
+
+def query(my_files, String):
+    #will take an array parameter of the files
+    String = ""
+    #this will be the string sent on to the client
+    for obj in my_files:
+        String = String + obj.name + "  " + str(obj.open) + "\n"
+    return String
+def save_file(CSV,file_name,ope,pin):
+        with open(CSV, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([file_name,ope,pin])
+
+def send_to_array(array):
+    with open("data/saved.csv", newline = '') as csvfile:
+        reader = csv.reader(csvfile,delimiter=',')
+
+        for row in reader:
+            ins_File = File(row[0],row[1],row[2])
+            array.append(ins_File)
+
+def search_array(arr, file_name):
+    for obj in arr:
+        if obj.name == file_name:
+            return obj
+    return None
+        
+if __name__ == '__main__':
     main()
